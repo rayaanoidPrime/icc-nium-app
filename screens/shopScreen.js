3ffc5f3e187179c9bb4c78212b1c430f11309187
imabhirupp <abhirupsinha0313@gmail.com>
2023-02-25 15:28:37 +0530

    Shop Screen UI

--- screens/shopScreen.js
+++ screens/shopScreen.js
@@ -1,9 +1,165 @@
-import { View, Text } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Pressable,
+  SafeAreaView,
+} from "react-native";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, decrementQuantity, incrementQuantity, removeFromCart } from "../CartReducer";
 
-export default function ShopScreen() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Shop Screen</Text>
-      </View>
-    );
+const HomeScreen = () => {
+  const cart = useSelector((state) => state.cart.cart);
+  console.log(cart);
+  const dispatch = useDispatch();
+  const images = [
+    {
+      id: "0",
+      image:
+        "content/dam/icicibank/india/managed-assets/images/offer-zone/shopping/gaames-merchandise-offer-d.jpg",
+      name: "Merchandise Kit",
+    },
+    {
+      id: "1",
+      image:
+        "https://www.sluggers-putters.com/wp-content/uploads/2019/06/Season_Pass_Red.png",
+      name: "Metapass",
+    },
+    {
+      id: "2",
+      image:
+        "https://i.pinimg.com/474x/ef/36/e2/ef36e24a16550237d67bdf2319322cf9.jpg",
+      name: "Ticket",
+    },
+    {
+      id: "3",
+      image:
+       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTI5UieCWPyqm51rVoEb3Mcw7Nm93_-wh1vw&usqp=CAU",
+       name: "ICC Points",
+    },
+  ];
+  const addItemToCart = (item) => {
+    dispatch(addToCart(item));
+  };
+  const removeItemFromCart = (item) => {
+    dispatch(removeFromCart(item));
+  };
+  const increaseQuantity = (item) => {
+    dispatch(incrementQuantity(item));
   }
+  const decreaseQuantity = (item) => {
+    if(item.quantity == 1){
+      dispatch(removeFromCart(item));
+    }else{
+      dispatch(decrementQuantity(item));
+    }
+  }
+  return (
+    <SafeAreaView>
+      <Text style={{ textAlign: "center", fontSize: 16 }}>
+        ICC Shop
+      </Text>
+      {images.map((item) => (
+        <Pressable
+          key={item.id}
+          style={{ flexDirection: "row", alignItems: "center" }}
+        >
+          <View style={{ margin: 10 }}>
+            <Image
+              style={{ width: 100, height: 100, borderRadius: 8 }}
+              source={{ uri: item.image }}
+            />
+          </View>
+          <View>
+            <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
+            {cart.some((value) => value.id == item.id) ? (
+              <Pressable onPress={() => removeItemFromCart(item)}>
+                <Text
+                  style={{
+                    borderColor: "gray",
+                    borderWidth: 1,
+                    marginVertical: 10,
+                    padding: 5,
+                  }}
+                >
+                  REMOVE FROM CART
+                </Text>
+              </Pressable>
+            ) : (
+              <Pressable onPress={() => addItemToCart(item)}>
+                <Text
+                  style={{
+                    borderColor: "gray",
+                    borderWidth: 1,
+                    marginVertical: 10,
+                    padding: 5,
+                  }}
+                >
+                  ADD TO CART
+                </Text>
+              </Pressable>
+            )}
+          </View>
+        </Pressable>
+      ))}
+
+      {cart.map((item,index) => (
+        <View style={{padding:10}} key={index}>
+          <Text>{item.name}</Text>
+          <Pressable
+            style={{
+              flexDirection: "row",
+              marginTop:20,
+              alignItems: "center",
+              backgroundColor: "#FF3366",
+              borderRadius: 5,
+              width: 120,
+            }}
+          >
+            <Pressable onPress={() => decreaseQuantity(item)}>
+              <Text
+                style={{
+                  fontSize: 25,
+                  color: "white",
+                  paddingHorizontal: 10,
+                }}
+              >
+                -
+              </Text>
+            </Pressable>
+
+            <Pressable>
+              <Text
+                style={{
+                  fontSize: 20,
+                  color: "white",
+                  paddingHorizontal: 10,
+                }}
+              >
+                {item.quantity}
+              </Text>
+            </Pressable>
+
+            <Pressable onPress={() => increaseQuantity(item)}>
+              <Text
+                style={{
+                  fontSize: 20,
+                  color: "white",
+                  paddingHorizontal: 10,
+                }}
+              >
+                +
+              </Text>
+            </Pressable>
+          </Pressable>
+        </View>
+      ))}
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
